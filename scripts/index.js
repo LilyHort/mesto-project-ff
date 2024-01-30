b2264b37b076dd182fbd778919fb38a07f8ea394
Лилиана Леднева <lilyledneva@gmail.com>
2024-01-30 15:07:28 +0300

    добавляет alt

--- scripts/index.js
+++ scripts/index.js
@@ -8,14 +8,15 @@ const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
-function createCard(item) {
+function createCard(item, deleteElement) {
     const card = cardTemplate.querySelector('.card').cloneNode(true);
     const deleteButton = card.querySelector(".card__delete-button");
 
     card.querySelector(".card__image").src = item.link;
+    card.querySelector(".card__image").alt = item.name;
     card.querySelector(".card__title").textContent = item.name;
 
-    deleteButton.addEventListener("click", function () {
+    deleteButton.addEventListener("click", function() {
         deleteElement(card);
     })
     return card;
