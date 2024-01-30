9c3c027688cd9fbb92b4984edac5136f416fe289
Лилиана Леднева <lilyledneva@gmail.com>
2024-01-30 14:03:38 +0300

    создает карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,39 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector("#card-template").content;
+
 // @todo: DOM узлы
+
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
+function createCard(item) {
+    const card = cardTemplate.querySelector('.card').cloneNode(true);
+    const deleteButton = card.querySelector(".card__delete-button");
+
+    card.querySelector(".card__image").src = item.link;
+    card.querySelector(".card__title").textContent = item.name;
+
+    deleteButton.addEventListener("click", function () {
+        deleteElement(card);
+    })
+    return card;
+}
+
 // @todo: Функция удаления карточки
 
+function deleteElement(card) {
+    card.remove()
+}
+
 // @todo: Вывести карточки на страницу
+
+function displayCards(arrayOfCards) {
+    arrayOfCards.forEach(function displayCards(item) {
+        const card = createCard(item);
+        placesList.appendChild(card);
+    });
+}
+
+displayCards(initialCards);
